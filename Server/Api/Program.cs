4d67a9b33a6e3019bb4bf6ee7813803208274ea2
bedev <thanhdaiu443@gmail.com>
2025-12-16 00:57:02 +0700

    update final

--- Server/Api/Program.cs
+++ Server/Api/Program.cs
@@ -36,12 +36,7 @@ builder.Services.AddCors(options =>
 
 builder.Services.AddDbContext<ClinicContext>(options =>
     options.UseSqlServer(
-        builder.Configuration.GetConnectionString("DbConnection"),
-        sql => sql.EnableRetryOnFailure(
-            maxRetryCount: 5,
-            maxRetryDelay: TimeSpan.FromSeconds(10),
-            errorNumbersToAdd: null
-        )
+        builder.Configuration.GetConnectionString("DbConnection")
     )
 );
 
